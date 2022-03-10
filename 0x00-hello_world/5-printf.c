fde45a71c04646c0cb61b224edb80bec34ff577a
root <chuksdav4@gmail.com>
2022-03-10 10:39:46 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** 
+ * Main entry point
+ *
+ * printing a statement
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
