e215144d75cb154a116bbed1cfbacd1642560639
bhirkoff <chuksdav4@gmail.com>
2022-03-22 01:12:45 +0100

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string,
+ * followed by a new line, to stdout.
+ *
+ * @str: string parameter passed
+ */
+
+void _puts(char *str)
+{
+	_putchar(str);
+	_putchar("\n");
+}
