a4e95e0447c36144b6d893cfbbf2706525c191ae
bhirkoff <chuksdav4@gmail.com>
2022-03-25 08:29:51 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _strncat - a function that concentrates two strings
+/**
+ * _strncat - a function that concentrates two strings
  * @dest: string 1
  * @src: string 2
  * @n: amount
@@ -12,9 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	{
 		;
-	}
+
 	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 	{
 		dest[i] = src[c];
