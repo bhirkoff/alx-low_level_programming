d5892458fd8628df6ef580e1678c070c54c3e789
bhirkoff <chuksdav4@gmail.com>
2022-03-25 08:48:28 +0100

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
 char *_strcat(char *dest, char *src);
-char *strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
