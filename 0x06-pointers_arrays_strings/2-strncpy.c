eaa245dcbc19e69996ffd492dd56adfbfa7ce22b
bhirkoff <chuksdav4@gmail.com>
2022-03-25 08:01:32 +0100

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,21 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		if (i < n)
-		{
-			dest[i] = src[i];
-		}
-	}
-
-	j = 1;
-
-	for (; n > j; j++)
-	{
-		dest[j] = '\0';
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
